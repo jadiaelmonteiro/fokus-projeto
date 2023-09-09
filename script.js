a2f6489db7687086f35c35dcf82b5beec6518f8f
jadiaelmonteiro <jadiael.monteiro@gmail.com>
2023-09-09 10:53:21 -0300

    Alter text with innerHtml

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const focoBt = document.querySelector('.app__card-button--foco');
 const shortBt = document.querySelector('.app__card-button--curto');
 const longBt = document.querySelector('.app__card-button--longo');
 const banner = document.querySelector('.app__image');
+const title = document.querySelector('.app__title');
 
 focoBt.addEventListener('click', () => {
     alterContent('foco');
@@ -20,4 +21,26 @@ longBt.addEventListener('click', () => {
 function alterContent(content) {
     html.setAttribute('data-contexto', content);
     banner.setAttribute('src', `/images/${content}.png`);
+    switch (content) {
+        case "foco":
+            title.innerHTML = `
+            Otimize sua produtividade, <br>
+                <strong class="app__title-strong">mergulhe no que importa.</strong>
+            `
+            break;
+        case "descanso-curto":
+            title.innerHTML = `
+            Que tal dar uma respirada? <br>
+                <strong class="app__title-strong">faça uma pausa curta!</strong>
+            `
+            break;
+        case "descanso-longo":
+            title.innerHTML = `
+            Hora de voltar à superficie. <br>
+                <strong class="app__title-strong">faça uma pausa longa!</strong>
+            `
+            break;
+        default:
+            break;
+    }
 }
