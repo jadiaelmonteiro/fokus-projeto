5e55dde469850e73c160bd40f5e244179705f1bd
jadiaelmonteiro <jadiael.monteiro@gmail.com>
2023-09-10 09:48:29 -0300

    Add timer and sounds

--- script.js
+++ script.js
@@ -5,9 +5,19 @@ const longBt = document.querySelector('.app__card-button--longo');
 const banner = document.querySelector('.app__image');
 const title = document.querySelector('.app__title');
 const buttons = document.querySelectorAll('.app__card-button');
+const btnPlayPause = document.querySelector('#start-pause');
 const musicFocoInput = document.querySelector('#alter-music');
 const music = new Audio('/sounds/luna-rise-part-one.mp3');
+const playTimer = new Audio('/sounds/play.wav');
+const pauseTimer = new Audio('/sounds/pause.mp3');
+const finishTimer = new Audio('/sounds/beep.mp3');
+
+
+
 music.loop = true;
+
+let timer = 5;
+let intervalId;
 
 musicFocoInput.addEventListener('change', () => {
     if (music.paused) {
@@ -61,3 +71,34 @@ function alterContent(content) {
             break;
     }
 }
+
+const countdownTimer = () => {
+    if (timer <= 0) {
+        finishTimer.play();
+        clearTimer();
+        return
+    }
+    timer -= 1;
+    console.log('My timer ' + timer);
+}
+
+btnPlayPause.addEventListener('click', playOrPauseTimer);
+
+function playOrPauseTimer() {
+
+    if (intervalId) {
+        pauseTimer.play();
+        clearTimer();
+        return
+    }
+    playTimer.play();
+    intervalId = setInterval(countdownTimer, 1000);
+}
+
+function clearTimer() {
+    clearInterval(intervalId);
+    intervalId = null;
+    timer = 5;
+}
+
+
