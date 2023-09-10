e942d94035e2ddfb1c73adad86cf82364f038899
jadiaelmonteiro <jadiael.monteiro@gmail.com>
2023-09-10 10:20:13 -0300

    Add timer in the screen

--- script.js
+++ script.js
@@ -6,17 +6,17 @@ const banner = document.querySelector('.app__image');
 const title = document.querySelector('.app__title');
 const buttons = document.querySelectorAll('.app__card-button');
 const btnPlayPause = document.querySelector('#start-pause');
+const labelBtnPlayPause = document.querySelector('#start-pause span');
+const imgBtnPlayPause = document.querySelector('#start-pause img');
 const musicFocoInput = document.querySelector('#alter-music');
+const timeForScreen = document.querySelector('#timer');
 const music = new Audio('/sounds/luna-rise-part-one.mp3');
 const playTimer = new Audio('/sounds/play.wav');
 const pauseTimer = new Audio('/sounds/pause.mp3');
 const finishTimer = new Audio('/sounds/beep.mp3');
-
-
-
 music.loop = true;
 
-let timer = 5;
+let timer = 1500;
 let intervalId;
 
 musicFocoInput.addEventListener('change', () => {
@@ -28,16 +28,22 @@ musicFocoInput.addEventListener('change', () => {
 });
 
 focoBt.addEventListener('click', () => {
+    timer = 1500;
+    showTimer();
     alterContent('foco');
     focoBt.classList.add('active');
 });
 
 shortBt.addEventListener('click', () => {
+    timer = 300;
+    showTimer();
     alterContent('descanso-curto');
     shortBt.classList.add('active');
 });
 
 longBt.addEventListener('click', () => {
+    timer = 900;
+    showTimer();
     alterContent('descanso-longo');
     longBt.classList.add('active');
 });
@@ -79,7 +85,7 @@ const countdownTimer = () => {
         return
     }
     timer -= 1;
-    console.log('My timer ' + timer);
+    showTimer();
 }
 
 btnPlayPause.addEventListener('click', playOrPauseTimer);
@@ -88,17 +94,32 @@ function playOrPauseTimer() {
 
     if (intervalId) {
         pauseTimer.play();
+        alterNameBtnPlayOrPause('Começar', 'play_arrow');
         clearTimer();
         return
     }
     playTimer.play();
+    alterNameBtnPlayOrPause('Pausar', 'pause');
     intervalId = setInterval(countdownTimer, 1000);
 }
 
 function clearTimer() {
     clearInterval(intervalId);
     intervalId = null;
-    timer = 5;
 }
+
+function alterNameBtnPlayOrPause(btnName, img) {
+    labelBtnPlayPause.textContent = btnName;
+    imgBtnPlayPause.setAttribute('src', `images/${img}.png`)
+}
+
+function showTimer() {
+    const time = new Date(timer * 1000);
+    const timeFormated = time.toLocaleTimeString('pt-br', { minute: '2-digit', second: '2-digit' });
+
+    timeForScreen.innerHTML = `${timeFormated}`;
+}
+
+showTimer();
 
 
